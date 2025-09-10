2bd916cd6fa8a011251281626e62a6e4d3f1a986
Rizki Citra <rimzzlabs@proton.me>
2025-09-10 17:31:20 +0700

    fix news list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,19 @@ import { Title } from "@/components/title";
 import { getServerNews } from "@/modules/news/query";
 import { createClient } from "@/modules/supabase/server";
 import { NewsList } from "./components/news-list";
+import { toInt } from "radash";
 
-export default async function Home() {
+export default async function Home(props: TPageProps) {
+  let params = await props.searchParams;
   let client = await createClient();
-  let newsQuery = await getServerNews(client, { status: "published" });
+
+  let page = toInt(params.page, 1);
+
+  let newsQuery = await getServerNews(client, {
+    status: "published",
+    limit: 10,
+    page,
+  });
 
   return (
     <div className="pt-8 pb-8">
@@ -13,7 +22,12 @@ export default async function Home() {
         Berita Terkini<span className="sr-only">Kabayan News</span>
       </Title>
 
-      <NewsList news={newsQuery.data ?? []} />
+      <NewsList
+        news={newsQuery.data ?? []}
+        count={newsQuery.count ?? 0}
+        page={page}
+        limit={10}
+      />
     </div>
   );
 }
